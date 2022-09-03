79f14a9b2fe0216d31654239605922073fec2155
Emtinan <emtinan.s.e.osman@gmail.com>
2022-09-03 14:57:37 +0200

    fixed bugs, added minimum sample size calculator

--- scripts/ABTestingFunctions.py
+++ scripts/ABTestingFunctions.py
@@ -79,6 +79,49 @@ class ABTesting:
         """
             Returns the p_value for an A/B test
         """
-        return scs.binom(Control, p_A).pmf(p_B * Exposed)
+        #return scs.binom(Control, p_A).pmf(p_B * Exposed)
+        #Since P-value is defined as the probability of observing an outcome as extreme or more extreme than the one observed, 
+        #assuming that the null hypothesis is true. The answer above is wrong.
+        #Null is a normal distribution, mean 0 and std = pooled_SE()
+        
+        d_hat = p_B -p_A
+        stderr = self.pooled_SE(Control, Exposed, p_A*Control, p_B* Exposed)
+        dist = self.ab_dist(stderr, mde= d_hat, group_type='control')
+        pvalue = (1 - dist.cdf(d_hat))*2
+        
+        return pvalue
     
-    
+    def min_sample_size(self, bcr, mde, power=0.8, sig_level=0.05):
+        """
+        Returns the minimum sample size to set up a split test 
+        Arguments: 
+            bcr (float): probability of success for control, sometimes
+            referred to as baseline conversion rate
+            mde (float): minimum change in measurement between control
+            group and test group if alternative hypothesis is true, sometimes
+            referred to as minimum detectable effect
+            power (float): probability of rejecting the null hypothesis when the
+            null hypothesis is false, typically 0.8
+            sig_level (float): significance level often denoted as alpha,
+            typically 0.05
+        Returns:
+            min_N: minimum sample size (float)
+        References:
+            Stanford lecture on sample sizes
+            http://statweb.stanford.edu/~susan/courses/s141/hopower.pdf
+        """
+        # standard normal distribution to determine z-values
+        standard_norm = scs.norm(0, 1)
+        
+        # find Z_beta from desired power
+        Z_beta = standard_norm.ppf(power)
+        
+        # find Z_alpha
+        Z_alpha = standard_norm.ppf(1-sig_level/2)
+        
+        # average of probabilities from both groups
+        pooled_prob = (bcr + bcr+mde) / 2
+        
+        min_N = (2 * pooled_prob * (1 - pooled_prob) * (Z_beta + Z_alpha)**2 / mde**2)
+        
+        return min_N
